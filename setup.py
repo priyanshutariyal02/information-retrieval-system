acbff9d85d5b816c10112b78e8d17a48987a10e4
Priyanshu Tariyal <138609182+priyanshutariyal02@users.noreply.github.com>
2024-07-11 23:45:31 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Generative AI Project",
+    version='0.0.0',
+    author="Priyanshu Tariyal",
+    author_email="tariyalpriyanshu6@gmail.com",
+    package=find_packages(),
+    install_requires=[]
+
+)
